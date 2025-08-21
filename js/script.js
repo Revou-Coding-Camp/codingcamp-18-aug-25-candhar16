30e939cd61c1ce2fa9a729d506639dc201ef8503
candhar16 <cloverprojectgaming@gmail.com>
2025-08-21 22:51:56 +0700

    fix: update form inputs and add validation function for task input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+function validateForm() {
+    //DOM Form Elements
+    const input = document.getElementById("task-input");
+    const errorMessage = document.getElementById("due-date-input");
+
+    if(taskInput.value=== ""){
+    alert("Please enter a task");
+    return false;
+}
+}
