b9fea2ec1de09d95ca21e20d75e34a826d74e776
candhar16 <cloverprojectgaming@gmail.com>
2025-08-21 23:32:38 +0700

    fix: enhance form validation for task and due date inputs

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ function validateForm() {
     const input = document.getElementById("task-input");
     const errorMessage = document.getElementById("due-date-input");
 
-    if(taskInput.value=== ""){
+    if(taskInput.value === ""){
     alert("Please enter a task");
     return false;
 }
+    if(dueDateInput.value === ""){
+        alert("Please enter a due date");
+        return false;
+    }
+
+   
 }
