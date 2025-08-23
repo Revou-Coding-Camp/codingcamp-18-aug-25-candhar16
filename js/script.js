501c4082504ec735142df0c85a935ff3894b5ab0
candhar16 <cloverprojectgaming@gmail.com>
2025-08-23 10:21:28 +0700

    fix: add initial CSS styles for body layout

--- js/script.js
+++ js/script.js
@@ -7,8 +7,37 @@ function validateForm() {
 
     if(taskInput.value === ""|| duedateInput.value === "") {
     alert("Please enter a task");
-    return false;
-}
+    } else {
+        addTodo(taskInput.value, dueDateInput.value);
+    }
 
    
+}
+
+
+
+/// Add a new Todo
+function addTodo(task, dueDate) {
+    listTodo.push({
+        task: task,
+        dueDate: dueDate
+    });
+
+    renderTodoList();
+}
+
+/// Render the Todo List
+function renderTodoList() {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = '';
+
+
+    for (let i = 0; i < listTodo.length; i++) {
+        taskList.innerHTML += `<li class="border-b py-2">${listTodo[i].task} - Due: ${listTodo[i].dueDate}</li>`;
+    }
+}
+/// Delete all Todos
+function deleteAll() {
+    listTodo = [];
+    renderTodoList();
 }
